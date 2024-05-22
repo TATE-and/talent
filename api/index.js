4cf3638df41d2f31094c3de5f9c2a5452ae12337
TATE-and <168579244+TATE-and@users.noreply.github.com>
2024-05-23 01:21:57 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,15 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/user', (req, res) => (
-  const user = {
-  id:  "1",
-  name: "Tate",
-  email: "tate05nta@gmail.com"
-  }
-  res.json(user))
-))
-
 app.listen(port, () => {
   console.log(`server  running on port ${port}`)
 })
